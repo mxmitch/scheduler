0e2d3587c9377bff154ba5d7773e71269f6bbce0
Mitch Lum <mitchlum@Mitchs-MacBook-Pro.local>
2020-01-23 16:55:24 -0800

    added and implemented replace function for transition in useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,16 +1,26 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) { 
-    setMode(newMode)
-    history.push(newMode)
+  function transition(newMode, replace) { 
+    if (!replace || replace === false) {
+      setMode(newMode)
+      history.push(newMode)
+    } else {
+      history.splice(history.length - 1, 1 ,newMode)
+      setMode(history[history.length-1]);
+    }
   }
+  
   function back() {
-    history.pop(); 
-    setMode(history[history.length - 1]);
+    if (history.length >= 2) { 
+      history.pop(); 
+      setMode(history[history.length - 1]);
+    } else {
+      setMode(history[0])
+    }
   }
 
   return { mode, transition, back };
